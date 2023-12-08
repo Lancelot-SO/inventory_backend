f7bbb131f929a174dd8d11958cc8c00703724896
Felix Sowah <60888274+Lancelot-SO@users.noreply.github.com>
2023-12-08 09:39:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://inventory-app-kohl.vercel.app"],
+    origin: ["http://localhost:3000", "https://inventory-app-alqw.onrender.com"],
     credentials: true,
   })
 );
