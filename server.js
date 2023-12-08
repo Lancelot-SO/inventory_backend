c33ff14799fe9864152cd749ba38a9f0ed33dab4
Lancelot-SO <fsowah001@gmail.com>
2023-12-08 01:46:49 +0000

    initial commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://inventory-app-ten-mu.vercel.app"],
+    origin: ["http://localhost:3000", "https://inventory-app-eight-livid.vercel.app"],
     credentials: true,
   })
 );
